36894410e40075cef4a5081b58d2627804b48cb2
bystritskiy123 <gor_ell@mail.ru>
2019-03-10 21:36:56 +0300

    v2.2

---
+++ catalog/js/custom.js
@@ -0,0 +1,30 @@
+// Slider
+$(document).ready(function () {
+	$('.slider').glide({
+		autoplay: 3500,
+		hoverpause: true,
+		arrowRightText: '&rarr;',
+		arrowLeftText: '&larr;'
+	})
+});
+
+// Scrolling button
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(window).scrollTop() >= 300) {
+			$('.gul-up').show('fast');
+		} else {
+			$('.gul-up').hide('fast');
+		}
+	});
+	if ($(window).scrollTop() >= 300) {
+		$('.gul-up').css('display', 'block');
+	} else {
+		$('.gul-up').css('display', 'none');
+	}
+	$('.gul-up').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+	});
+});
---
+++ catalog/js/customForSingleGood.js
@@ -0,0 +1,52 @@
+// Scrolling button
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(window).scrollTop() >= 300) {
+			$('.gul-up').show('fast');
+		} else {
+			$('.gul-up').hide('fast');
+		}
+	});
+	if ($(window).scrollTop() >= 300) {
+		$('.gul-up').css('display', 'block');
+	} else {
+		$('.gul-up').css('display', 'none');
+	}
+	$('.gul-up').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
+	});
+});
+
+//Switcher+zoom
+$(document).ready(function () {
+	if ($(window).width() > 768) {
+		$('.reduced-image img').click(function () {
+			var attr = $(this).attr('src');
+			$('.main-image img').attr('src', attr);
+			$('.fancy-link a').attr('href', attr);
+			$('.zoom-effect').trigger('zoom.destroy');
+			$('.zoom-effect').zoom();
+		})
+	} else {
+		$('.reduced-image img').click(function () {
+			var attr = $(this).attr('src');
+			$('.main-image img').attr('src', attr);
+			$('.fancy-link a').attr('href', attr);
+		})};
+	});
+
+//Zoom
+$(document).ready(function () {
+	if ( $(window).width() > 768 ){
+  	$('.zoom-effect').zoom();
+}});
+
+//Tabs
+$(document).ready(function () {
+	if ( $(window).width() <= 650 ){
+		$('.toogle-tab').click(function() {
+			$(this).toggleClass('in').next().slideToggle(300);
+		})
+}});
